f884e66cee42e9c98a3cb073b7c1c3f57317f38e
Raymond Woo <raymondhfwoo@gmail.com>
2022-08-30 11:20:26 +0800

    Added blog and static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,11 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = ['./portfolio/static/']
 
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,9 +19,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import include
+
 import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
